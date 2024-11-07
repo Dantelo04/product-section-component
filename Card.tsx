62822c7c572a21120cd03df51b25a7ddb553eefb
Dante <dante.rivadi@gmail.com>
2024-11-07 17:59:03 -0300

    Responsive changes

--- Card.tsx
+++ Card.tsx
@@ -15,11 +15,11 @@ export const Card = ({title,description, children,link}:CardProps) => {
     const [hover, setHover] = useState(false)
 
     return(
-        <a href={link ? link:"#"} className="px-6 rounded-xl h-[400px] w-64 py-10 shadow relative duration-200 ease" onMouseEnter={()=>setHover(true)} onMouseLeave={()=>setHover(false)}>
-            <div className="flex flex-col gap-3 relative h-full">
+        <a href={link ? link:"#"} className="px-6 rounded-xl min-h-[400px] xl:w-64 w-full py-10 shadow relative duration-200 ease" onMouseEnter={()=>setHover(true)} onMouseLeave={()=>setHover(false)}>
+            <div className="flex flex-col xl:gap-3 gap-4 relative h-full xl:items-start items-center">
                 {children}
                 <h2>{title ? title:"Here goes the title"}</h2>
-                <ul className="flex flex-col gap-3">
+                <ul className="flex flex-col gap-3 pb-10 xl:pb-0 xl:w-fit md:w-4/12">
                     {description ? description?.map((item)=>
                         <li key={item}>
                             <IoMdCheckmark className="w-4 h-4 text-secondary"/><span className="text">{item}</span>
--- ProductSection.tsx
+++ ProductSection.tsx
@@ -8,15 +8,15 @@ export const ProductSection = () => {
 
     return(
         <section className="flex justify-center">
-            <div className="flex flex-col justify-center items-center gap-10 xl:w-8/12">
+            <div className="flex flex-col justify-center items-center gap-10 xl:w-8/12 px-5">
                 <div className="xl:w-7/12">
                     <h1 className="">Drive results across every stage of the Insurance Life Cycle</h1>
                 </div>
-                <div className="flex xl:flex-row flex-col gap-4">
+                <div className="flex xl:flex-row flex-col gap-4 w-full justify-center">
                     <Card
                         title="Customer Relationship Management"
                         description={["Develop Strong Agent Relationships","Contact the right agents","Put your data to work"]}
-                    ><LuUsers className="w-6 h-auto text-primary"/>
+                    ><LuUsers className="xl:w-6 w-7 h-auto text-primary"/>
                     </Card>
                     <Card
                         title="Distribution Management System"
@@ -25,7 +25,7 @@ export const ProductSection = () => {
                             "Simplify communication and elevate your service",
                             "Get organized and reduce mistakes"
                         ]}
-                    ><BiNetworkChart className="w-6 h-auto text-primary"/>
+                    ><BiNetworkChart className="xl:w-6 w-7 h-auto text-primary"/>
                     </Card>
                     <Card
                         title="Incentives and Commissions Management"
@@ -34,7 +34,7 @@ export const ProductSection = () => {
                             "Monitor your commissions",
                             "Keep agents motivated with trips, marketing credits and incentives"
                         ]}
-                    ><PiHandCoinsBold className="w-6 h-auto text-primary"/>
+                    ><PiHandCoinsBold className="xl:w-6 w-7 h-auto text-primary"/>
                     </Card>
                 </div>
             </div>
