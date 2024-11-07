e7da1e91eb3a03f1b341294254e4c367585e36f9
Dante <dante.rivadi@gmail.com>
2024-11-07 18:12:00 -0300

    Component with no icons

---
+++ ProductSectionNoIcons.tsx
@@ -0,0 +1,40 @@
+import "./local.css"
+import { Card } from "./Card"
+
+export const ProductSection = () => {
+
+    return(
+        <section className="flex justify-center">
+            <div className="flex flex-col justify-center items-center gap-10 xl:w-8/12 px-5">
+                <div className="xl:w-7/12">
+                    <h1 className="">Drive results across every stage of the Insurance Life Cycle</h1>
+                </div>
+                <div className="flex xl:flex-row flex-col gap-4 w-full justify-center">
+                    <Card
+                        title="Customer Relationship Management"
+                        description={["Develop Strong Agent Relationships","Contact the right agents","Put your data to work"]}
+                    ><div className="xl:w-6 w-7 h-auto rounded-full bg-primary"/>
+                    </Card>
+                    <Card
+                        title="Distribution Management System"
+                        description={[
+                            "Streamline your processes and drop the busy work",
+                            "Simplify communication and elevate your service",
+                            "Get organized and reduce mistakes"
+                        ]}
+                    ><div className="xl:w-6 w-7 h-auto rounded-full bg-primary"/>
+                    </Card>
+                    <Card
+                        title="Incentives and Commissions Management"
+                        description={[
+                            "Reduce stress and increase profits",
+                            "Monitor your commissions",
+                            "Keep agents motivated with trips, marketing credits and incentives"
+                        ]}
+                    ><div className="xl:w-6 w-7 h-auto rounded-full bg-primary"/>
+                    </Card>
+                </div>
+            </div>
+        </section>
+    )
+}
